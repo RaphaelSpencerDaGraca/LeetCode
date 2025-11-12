acc683fc608fae45d38a128cced98a9af2df4498
RaphaelSpencerDaGraca <oversquad04@gmail.com>
2025-11-12 01:30:36 +0100

    Ajout de validPalindromes

---
+++ TwoPointers/validPalindromes.py
@@ -0,0 +1,18 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        str_cleaned = re.sub(r"[^a-zA-Z0-9]","",s).lower()
+        return str_cleaned == str_cleaned[::-1]
+    
+
+
+def test_validPalindromes():
+    sol = Solution()
+
+    assert sol.isPalindrome("A man, a plan, a canal: Panama") == True
+
+
+
+if __name__ == "__main__":
+    test_validPalindromes()
