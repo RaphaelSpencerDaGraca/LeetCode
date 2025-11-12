e4cf7d6f87207aa43adf77fe621c7d3545651431
RaphaelSpencerDaGraca <165330721+RaphaelSpencerDaGraca@users.noreply.github.com>
2025-11-12 12:53:10 +0100

    Update containersWithMostWater.py

---
+++ TwoPointers/containersWithMostWater.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left,right = 0,len(height)-1
+        result = 0
+        for i in range (len(height)-1):
+            result = max(result, min(height[left],height[right])*(right-left))
+            if (height[left]< height[right]):
+                left +=1
+            else:
+                right-=1      
+        return result
+    
+
+def test_containersWithMosterWater():
+    sol = Solution()
+
+    assert sol.maxArea([1,8,6,2,5,4,8,3,7]) == 49
+    assert sol.maxArea([1,1]) == 1
+
+if __name__ =="__main__":
+    test_containersWithMosterWater()
